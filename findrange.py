a286c2b8265835ee28f9ba0e9cfa560a1df70476
Rudi Chen <rudichen@gmail.com>
2016-04-27 15:20:08 -0400

    Maximize 3D Perlin Noise and gradient magnitude.

--- findrange.py
+++ findrange.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import math
 import random
 
 from autograd import grad
@@ -7,6 +8,12 @@ from math import pi, degrees, radians
 from scipy.optimize import minimize
 
 from perlin import *
+
+# Return a theta and psi representing a vector on a unit sphere distributed
+# such that they correctly distribute points over a unit sphere.
+# http://mathworld.wolfram.com/SpherePointPicking.html
+def sphere_random():
+    return (random.uniform(0, 2 * pi), math.acos(random.uniform(-1, 1)))
 
 def perlin2d_gradient_descent(tolerance, iterations, fn):
     results = {}
@@ -47,6 +54,52 @@ def perlin2d_gradient_descent(tolerance, iterations, fn):
     # Return the best results
     return sorted((val, key) for key, val in results.iteritems())
 
+def perlin3d_gradient_descent(tolerance, iterations, fn):
+    results = {}
+
+    # optimize.minimize and autograd.grad require a function that
+    # takes in an array of arguments
+    gradient = grad(fn)
+
+    for i in range(iterations):
+        if i % 20 == 0:
+            print "Minimization %d" % i
+
+        # Random starting values
+        t_bll, p_bll = sphere_random()
+        t_blr, p_blr = sphere_random()
+        t_bul, p_bul = sphere_random()
+        t_bur, p_bur = sphere_random()
+        t_tll, p_tll = sphere_random()
+        t_tlr, p_tlr = sphere_random()
+        t_tul, p_tul = sphere_random()
+        t_tur, p_tur = sphere_random()
+
+        x    = random.uniform(0, 1)
+        y    = random.uniform(0, 1)
+        z    = random.uniform(0, 1)
+
+        x0 = [t_bll, p_bll, t_blr, p_blr, t_bul, p_bul, t_bur, p_bur,
+              t_tll, p_tll, t_tlr, p_tlr, t_tul, p_tul, t_tur, p_tur,
+              x, y, z]
+
+        result = minimize(
+            fn,
+            x0,
+            bounds=((0, 2 * pi), (0, pi)) * 8 + ((0, 1),) * 3,
+            method='L-BFGS-B',
+            jac=gradient,
+            tol=tolerance)
+
+        key = tuple(round(var, 3) for var in result.x)
+        if key in results:
+            results[key] = min(results[key], result.fun)
+        else:
+            results[key] = result.fun
+
+    # Return the best results
+    return sorted((val, key) for key, val in results.iteritems())
+
 def perlin2d_gradient_ascent(tolerance, iterations, fn):
     best = perlin2d_gradient_descent(tolerance, iterations, lambda args: -fn(args))
 
@@ -62,6 +115,20 @@ def perlin2d_gradient_ascent(tolerance, iterations, fn):
                      x, y)
         print "    also %.3fpi %.3fpi %.3fpi %.3fpi" % (a_ll / pi, a_lr / pi, a_ul / pi, a_ur / pi)
 
+def perlin3d_gradient_ascent(tolerance, iterations, fn):
+    best = perlin3d_gradient_descent(tolerance, iterations, lambda args: -fn(args))
+
+    largest_smaller = next(index for (index, (val, key))
+                                 in enumerate(best)
+                                 if round(val, 5) > round(best[0][0], 5))
+
+    for val, key in best[:max(6, largest_smaller + 2)]:
+        t_bll, p_bll, t_blr, p_blr, t_bul, p_bul, t_bur, p_bur = key[0:8]
+        t_tll, p_tll, t_tlr, p_tlr, t_tul, p_tul, t_tur, p_tur = key[8:16]
+        x, y, z = key[16:19]
+        print -val, [round(degrees(angle), 2) for angle in key[0:16]], (x, y, z)
+        print "    also {}pi".format([round(angle / pi, 3) for angle in key[0:16]])
+
 print ">>> Maximize 2D Perlin Noise with 3rd order interpolant"
 perlin2d_gradient_ascent(1e-10, 100, lambda args: perlin2D(easing3)(*args))
 print ">>> Maximize 2D Perlin Noise with 5th order interpolant"
@@ -70,3 +137,12 @@ print ">>> Maximize 2D Perlin Noise's Gradient Magnitude with 3rd order interpol
 perlin2d_gradient_ascent(1e-10, 200, perlin2D_gradient_magnitude(easing3))
 print ">>> Maximize 2D Perlin Noise's Gradient Magnitude with 5th order interpolant"
 perlin2d_gradient_ascent(1e-10, 200, perlin2D_gradient_magnitude(easing5))
+
+print ">>> Maximize 3D Perlin Noise with 3rd order interpolant"
+perlin3d_gradient_ascent(1e-10, 200, lambda args: perlin3D(easing3)(*args))
+print ">>> Maximize 3D Perlin Noise with 5th order interpolant"
+perlin3d_gradient_ascent(1e-10, 200, lambda args: perlin3D(easing5)(*args))
+print ">>> Maximize 3D Perlin Noise's Gradient Magnitude with 3rd order interpolant"
+perlin3d_gradient_ascent(1e-10, 400, perlin3D_gradient_magnitude(easing3))
+print ">>> Maximize 3D Perlin Noise's Gradient Magnitude with 5th order interpolant"
+perlin3d_gradient_ascent(1e-10, 400, perlin3D_gradient_magnitude(easing5))
--- perlin.py
+++ perlin.py
@@ -21,6 +21,9 @@ def easing5(t):
 def unit_vector(angle):
     return (np.cos(angle), np.sin(angle))
 
+def unit_vector3d(theta, psi):
+    return (np.cos(theta) * np.sin(psi), np.sin(theta) * np.sin(psi), np.cos(psi))
+
 def perlin2D(easing):
     def f(angle_lower_left, angle_lower_right,
           angle_upper_left, angle_upper_right, x, y):
@@ -35,10 +38,55 @@ def perlin2D(easing):
                     y_interp)
     return f
 
-def perlin2D_gradient_magnitude(easing):
+def perlin3D(easing):
+    def f(theta_bottom_lower_left,  psi_bottom_lower_left,
+          theta_bottom_lower_right, psi_bottom_lower_right,
+          theta_bottom_upper_left,  psi_bottom_upper_left,
+          theta_bottom_upper_right, psi_bottom_upper_right,
+          theta_top_lower_left,  psi_top_lower_left,
+          theta_top_lower_right, psi_top_lower_right,
+          theta_top_upper_left,  psi_top_upper_left,
+          theta_top_upper_right, psi_top_upper_right,
+          x, y, z):
+        x_interp = easing(x)
+        y_interp = easing(y)
+        z_interp = easing(z)
+        bottom_lower_left = np.dot(
+            unit_vector3d(theta_bottom_lower_left, psi_bottom_lower_left), [x, y, z])
+        bottom_lower_right = np.dot(
+            unit_vector3d(theta_bottom_lower_right, psi_bottom_lower_right), [x - 1.0, y, z])
+        bottom_upper_left = np.dot(
+            unit_vector3d(theta_bottom_upper_left, psi_bottom_upper_left), [x, y - 1.0, z])
+        bottom_upper_right = np.dot(
+            unit_vector3d(theta_bottom_upper_right, psi_bottom_upper_right), [x - 1.0, y - 1.0, z])
+        top_lower_left = np.dot(
+            unit_vector3d(theta_top_lower_left, psi_top_lower_left), [x, y, 1.0 - z])
+        top_lower_right = np.dot(
+            unit_vector3d(theta_top_lower_right, psi_top_lower_right), [x - 1.0, y, 1.0 - z])
+        top_upper_left = np.dot(
+            unit_vector3d(theta_top_upper_left, psi_top_upper_left), [x, y - 1.0, 1.0 - z])
+        top_upper_right = np.dot(
+            unit_vector3d(theta_top_upper_right, psi_top_upper_right), [x - 1.0, y - 1.0, 1.0 - z])
+        return lerp(lerp(lerp(bottom_lower_left, bottom_lower_right, x_interp),
+                         lerp(bottom_upper_left, bottom_upper_right, x_interp),
+                         y_interp),
+                    lerp(lerp(top_lower_left, top_lower_right, x_interp),
+                         lerp(top_upper_left, top_upper_right, x_interp),
+                         y_interp),
+                    z_interp)
+
+    return f
+
+def gradient_magnitude(fn):
     # Take the gradient w.r.t to an _array_ of arguments.
-    gradient_function = grad(lambda args: perlin2D(easing)(*args))
+    gradient_function = grad(lambda args: fn(*args))
     def f(args):
         gradient = list(gradient_function(args))
         return np.sqrt(np.dot(gradient, gradient))
     return f
+
+def perlin2D_gradient_magnitude(easing):
+    return gradient_magnitude(perlin2D(easing))
+
+def perlin3D_gradient_magnitude(easing):
+    return gradient_magnitude(perlin3D(easing))
